a760705617c61f4568641f973bb887b15eca9843
Ondřej Mejzlík <mejzlik.ondrej@gmail.com>
2019-11-08 13:06:29 +0100

    Further gui experiments

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Python rewrite of the white bear website editor.
 TODO:
 Use wxpython for gui
 Use sqlite for storing configuration
+https://wiki.wxpython.org/Getting%20Started
--- TESTING/Gui.py
+++ TESTING/Gui.py
@@ -11,12 +11,26 @@ class Gui(wx.Frame):
         self.Centre()
         self.Update()
 
+        # Create menu bar and the menu itself
         menu_bar = wx.MenuBar()
         file_menu = wx.Menu()
-        file_menu_item = file_menu.Append(wx.ID_EXIT, 'Quit', 'Quit')
+        # Add the file menu into the menu bar. & Tells the program to create Ctrl+F shortcut to open menu.
         menu_bar.Append(file_menu, '&File')
         self.SetMenuBar(menu_bar)
-        self.Bind(wx.EVT_MENU, self.quit_button_handler, file_menu_item)
+
+        # Create a menu item
+        file_menu_item_quit = wx.MenuItem(file_menu, wx.ID_EXIT, '&Quit', 'Any item help')
+        file_menu.Append(file_menu_item_quit)
+
+        # Create a base panel
+        main_panel = wx.Panel(self)
+
+
+        # Create a list view
+
+
+        # Bind click handlers
+        self.Bind(wx.EVT_MENU, self.quit_button_handler, file_menu_item_quit)
 
     def quit_button_handler(self, event):
         self.Close()
