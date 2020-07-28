71f874cac57be4080c83a7e043a32469cf20f5ad
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2020-07-28 19:23:14 +0200

    Switch from mnemonics to shortcuts

--- Constants/Strings.py
+++ Constants/Strings.py
@@ -26,15 +26,15 @@ class Strings:
     exception_conf_inaccessible: str = '.whitebearEditor.conf inaccessible'
     exception_resource_not_found: str = 'Resource not found'
 
-    label_menu_file: str = '&File'
-    label_menu_item_open: str = '&Open directory'
+    label_menu_file: str = 'File'
+    label_menu_item_open: str = 'Open directory\tctrl+o'
     label_menu_item_open_hint: str = 'Open a whitebear web directory'
 
-    label_menu_item_quit: str = '&Quit'
+    label_menu_item_quit: str = 'Quit\tctrl+q'
     label_menu_item_quit_hint: str = 'Quit editor'
 
-    label_menu_help: str = '&Help'
-    label_menu_item_about: str = '&About'
+    label_menu_help: str = 'Help'
+    label_menu_item_about: str = 'About'
     label_menu_item_about_hint: str = 'About whitebear editor'
 
     label_dialog_choose_wb_dir: str = 'Choose whitebear directory'
