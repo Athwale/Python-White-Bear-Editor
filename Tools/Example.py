f52caa4de71a4bd325627a5b6d0e40e4d246e67e
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2020-12-07 16:29:15 +0100

    Example for another question

--- Tools/Example.py
+++ Tools/Example.py
@@ -1,5 +1,4 @@
 import wx
-import wx.html
 import wx.richtext as rt
 
 
@@ -35,14 +34,12 @@ class RichTextFrame(wx.Frame):
         """
         # Paragraph style
         stl_paragraph: rt.RichTextAttr = rt.RichTextAttr()
-        stl_paragraph.SetFlags(
-            wx.TEXT_ATTR_FONT_SIZE | wx.TEXT_ATTR_ALIGNMENT | wx.TEXT_ATTR_FONT_WEIGHT | wx.TEXT_ATTR_PARA_SPACING_AFTER
-            | wx.TEXT_ATTR_PARA_SPACING_BEFORE)
         stl_paragraph.SetFontSize(Numbers.paragraph_font_size)
         stl_paragraph.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
         stl_paragraph.SetFontWeight(wx.FONTWEIGHT_NORMAL)
-        stl_paragraph.SetParagraphSpacingBefore(0)
-        stl_paragraph.SetParagraphSpacingAfter(0)
+        stl_paragraph.SetParagraphSpacingBefore(Numbers.paragraph_spacing)
+        stl_paragraph.SetParagraphSpacingAfter(Numbers.paragraph_spacing)
+        stl_paragraph.SetBackgroundColour(wx.RED)
 
         style_paragraph: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
             Strings.style_paragraph)
@@ -50,51 +47,20 @@ class RichTextFrame(wx.Frame):
         style_paragraph.SetNextStyle(Strings.style_paragraph)
         self._stylesheet.AddParagraphStyle(style_paragraph)
 
-        # Paragraph style 1
-        stl_paragraph_1: rt.RichTextAttr = rt.RichTextAttr()
-        stl_paragraph_1.SetFlags(
-            wx.TEXT_ATTR_FONT_SIZE | wx.TEXT_ATTR_ALIGNMENT | wx.TEXT_ATTR_FONT_WEIGHT | wx.TEXT_ATTR_PARA_SPACING_AFTER
-            | wx.TEXT_ATTR_PARA_SPACING_BEFORE)
-        stl_paragraph_1.SetFontSize(Numbers.paragraph_font_size_1)
-        stl_paragraph_1.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
-        stl_paragraph_1.SetFontWeight(wx.FONTWEIGHT_NORMAL)
-        stl_paragraph_1.SetParagraphSpacingBefore(0)
-        stl_paragraph_1.SetParagraphSpacingAfter(0)
+        # Paragraph style bold
+        stl_paragraph_bold: rt.RichTextAttr = rt.RichTextAttr()
+        stl_paragraph_bold.SetFontSize(Numbers.paragraph_font_size_1)
+        stl_paragraph_bold.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
+        stl_paragraph_bold.SetFontWeight(wx.BOLD)
+        stl_paragraph_bold.SetParagraphSpacingBefore(Numbers.paragraph_spacing)
+        stl_paragraph_bold.SetParagraphSpacingAfter(Numbers.paragraph_spacing)
+        stl_paragraph_bold.SetBackgroundColour(wx.BLUE)
 
-        style_paragraph_1: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
-            Strings.style_paragraph_1)
-        style_paragraph_1.SetStyle(stl_paragraph_1)
-        style_paragraph_1.SetNextStyle(Strings.style_paragraph)
-        self._stylesheet.AddParagraphStyle(style_paragraph_1)
-
-        # List style
-        stl_list: rt.RichTextAttr = rt.RichTextAttr()
-        stl_list.SetFlags(
-            wx.TEXT_ATTR_FONT_SIZE | wx.TEXT_ATTR_ALIGNMENT | wx.TEXT_ATTR_FONT_WEIGHT | wx.TEXT_ATTR_PARA_SPACING_AFTER
-            | wx.TEXT_ATTR_PARA_SPACING_BEFORE)
-        stl_list.SetFontSize(Numbers.paragraph_font_size)
-        stl_list.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
-        stl_list.SetFontWeight(wx.FONTWEIGHT_NORMAL)
-        stl_list.SetParagraphSpacingBefore(Numbers.list_spacing)
-        stl_list.SetParagraphSpacingAfter(Numbers.list_spacing)
-
-        stl_list_1: rt.RichTextAttr = rt.RichTextAttr()
-        stl_list_1.SetFlags(
-            wx.TEXT_ATTR_FONT_SIZE | wx.TEXT_ATTR_ALIGNMENT | wx.TEXT_ATTR_FONT_WEIGHT | wx.TEXT_ATTR_PARA_SPACING_AFTER
-            | wx.TEXT_ATTR_PARA_SPACING_BEFORE | wx.TEXT_ATTR_BULLET_STYLE | wx.TEXT_ATTR_LEFT_INDENT)
-        stl_list_1.SetFontSize(Numbers.paragraph_font_size)
-        stl_list_1.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
-        stl_list_1.SetFontWeight(wx.FONTWEIGHT_NORMAL)
-        stl_list.SetParagraphSpacingBefore(Numbers.list_spacing)
-        stl_list.SetParagraphSpacingAfter(Numbers.list_spacing)
-        stl_list_1.SetBulletStyle(wx.TEXT_ATTR_BULLET_STYLE_STANDARD)
-        stl_list_1.SetLeftIndent(Numbers.list_left_indent, Numbers.list_left_sub_indent)
-
-        style_list: rt.RichTextListStyleDefinition = rt.RichTextListStyleDefinition(Strings.style_list)
-        style_list.SetLevelAttributes(0, stl_list_1)
-        style_list.SetStyle(stl_list)
-        style_list.SetNextStyle(Strings.style_paragraph)
-        self._stylesheet.AddListStyle(style_list)
+        style_paragraph_bold: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
+            Strings.style_paragraph_bold)
+        style_paragraph_bold.SetStyle(stl_paragraph_bold)
+        style_paragraph_bold.SetNextStyle(Strings.style_paragraph)
+        self._stylesheet.AddParagraphStyle(style_paragraph_bold)
 
         self.rtc.SetStyleSheet(self._stylesheet)
         self._style_control.SetRichTextCtrl(self.rtc)
@@ -111,21 +77,10 @@ class RichTextFrame(wx.Frame):
         self.rtc.EndParagraphStyle()
         self.rtc.Newline()
 
-        self.rtc.BeginParagraphStyle(Strings.style_paragraph_1)
+        self.rtc.BeginParagraphStyle(Strings.style_paragraph_bold)
         self.rtc.WriteText('Example paragraph')
         self.rtc.EndParagraphStyle()
         self.rtc.Newline()
-
-        self.rtc.BeginParagraphStyle(Strings.style_paragraph)
-        self.rtc.WriteText('Example paragraph')
-        self.rtc.EndParagraphStyle()
-        self.rtc.Newline()
-
-        self.rtc.BeginListStyle(Strings.style_list, 0)
-        for i in range(5):
-            self.rtc.WriteText('Example list item')
-            self.rtc.WriteText('\n')
-        self.rtc.EndListStyle()
 
         self.rtc.BeginParagraphStyle(Strings.style_paragraph)
         self.rtc.WriteText('Example paragraph')
@@ -138,16 +93,13 @@ class Strings:
     Just constants
     """
     style_paragraph: str = 'paragraph'
-    style_paragraph_1: str = 'paragraph1'
-    style_list: str = 'list'
+    style_paragraph_bold: str = 'boldparagraph'
 
 
 class Numbers:
-    list_spacing: int = 10
     paragraph_font_size: int = 12
     paragraph_font_size_1: int = 16
-    list_left_indent: int = 30
-    list_left_sub_indent: int = 30
+    paragraph_spacing: int = 20
 
 
 class MyApp(wx.App):
