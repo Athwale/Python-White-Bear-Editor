b06b8e4f5402067ca13c1d4dfd86ae7de62d42d5
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2020-12-12 11:55:31 +0100

    Use the workaround

--- Tools/Example.py
+++ Tools/Example.py
@@ -17,8 +17,8 @@ class RichTextFrame(wx.Frame):
         self._style_control.SetStyleType(0)
         self._style_control.SetMargins(-5, -5)
 
-        self._button = wx.Button(self, -1, 'Apply bold style')
-        self.Bind(wx.EVT_BUTTON, self._style_button_handler, self._button)
+        self.style_button = wx.Button(self, wx.ID_BOLD, 'style button bold')
+        self.style_button_bold = wx.Button(self, wx.ID_DEFAULT, 'style button normal')
 
         self.rtc.SetStyleSheet(self._stylesheet)
         self._style_control.SetRichTextCtrl(self.rtc)
@@ -26,24 +26,14 @@ class RichTextFrame(wx.Frame):
 
         self.sizer.Add(self.rtc, 1, flag=wx.EXPAND)
         self.sizer.Add(self._style_control)
-        self.sizer.Add(self._button)
+        self.sizer.Add(self.style_button)
+        self.sizer.Add(self.style_button_bold)
         self.SetSizer(self.sizer)
 
+        self.Bind(wx.EVT_BUTTON, self._change_style, self.style_button)
+        self.Bind(wx.EVT_BUTTON, self._change_style, self.style_button_bold)
+
         self._create_styles()
-
-    def _style_button_handler(self, evt: wx.CommandEvent) -> None:
-        """
-        Handles button clicks.
-        :param evt: Not used
-        :return: None
-        """
-        style = self._stylesheet.FindParagraphStyle(Strings.style_paragraph_bold).GetStyle()
-        p: rt.RichTextParagraph = self.rtc.GetFocusObject().GetParagraphAtPosition(self.rtc.GetCaretPosition()+1)
-        self.rtc.SetStyleEx(p.GetRange().FromInternal(), style, flags=rt.RICHTEXT_SETSTYLE_RESET |
-                            rt.RICHTEXT_SETSTYLE_OPTIMIZE | rt.RICHTEXT_SETSTYLE_WITH_UNDO)
-
-        # This calls the SetStyleEx function later on according to c++ code
-        # self.rtc.ApplyStyle(self._stylesheet.FindParagraphStyle(Strings.style_paragraph_bold))
 
     def _create_styles(self) -> None:
         """
@@ -60,9 +50,6 @@ class RichTextFrame(wx.Frame):
         stl_paragraph.SetParagraphSpacingAfter(Numbers.paragraph_spacing)
         stl_paragraph.SetBackgroundColour(wx.GREEN)
         stl_paragraph.SetParagraphStyleName(Strings.style_paragraph)
-        stl_paragraph.SetFlags(wx.TEXT_ATTR_ALIGNMENT | wx.TEXT_ATTR_FONT_SIZE | wx.TEXT_ATTR_FONT_WEIGHT |
-                               wx.TEXT_ATTR_PARA_SPACING_AFTER | wx.TEXT_ATTR_PARA_SPACING_BEFORE |
-                               wx.TEXT_ATTR_BACKGROUND_COLOUR | wx.TEXT_ATTR_PARAGRAPH_STYLE_NAME ^ rt.RICHTEXT_SETSTYLE_PARAGRAPHS_ONLY ^ rt.RICHTEXT_SETSTYLE_CHARACTERS_ONLY)
 
         style_paragraph: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
             Strings.style_paragraph)
@@ -79,10 +66,6 @@ class RichTextFrame(wx.Frame):
         stl_paragraph_bold.SetParagraphSpacingAfter(Numbers.paragraph_spacing_bold)
         stl_paragraph_bold.SetBackgroundColour(wx.YELLOW)
         stl_paragraph.SetParagraphStyleName(Strings.style_paragraph_bold)
-        stl_paragraph_bold.SetFlags(wx.TEXT_ATTR_ALIGNMENT | wx.TEXT_ATTR_FONT_SIZE | wx.TEXT_ATTR_FONT_WEIGHT |
-                                    wx.TEXT_ATTR_PARA_SPACING_AFTER | wx.TEXT_ATTR_PARA_SPACING_BEFORE |
-                                    wx.TEXT_ATTR_BACKGROUND_COLOUR | wx.TEXT_ATTR_PARAGRAPH |
-                                    wx.TEXT_ATTR_PARAGRAPH_STYLE_NAME ^ rt.RICHTEXT_SETSTYLE_PARAGRAPHS_ONLY ^ rt.RICHTEXT_SETSTYLE_CHARACTERS_ONLY)
 
         style_paragraph_bold: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
             Strings.style_paragraph_bold)
@@ -96,26 +79,40 @@ class RichTextFrame(wx.Frame):
         self._style_control.UpdateStyles()
         self.rtc.SetDefaultStyle(self._stylesheet.FindParagraphStyle(Strings.style_paragraph).GetStyle())
 
+    def _change_style(self, evt: wx.CommandEvent) -> None:
+        """
+        Handles button clicks.
+        :param evt: The name of the style in stylesheet
+        :return: None
+        """
+        if evt.GetId() == wx.ID_BOLD:
+            name = Strings.style_paragraph_bold
+        else:
+            name = Strings.style_paragraph
+        style = self._stylesheet.FindParagraphStyle(name).GetStyle()
+        p: rt.RichTextParagraph = self.rtc.GetFocusObject().GetParagraphAtPosition(self.rtc.GetCaretPosition()+1)
+        self.rtc.SetStyleEx(p.GetRange().FromInternal(), style, flags=rt.RICHTEXT_SETSTYLE_RESET |
+                            rt.RICHTEXT_SETSTYLE_OPTIMIZE | rt.RICHTEXT_SETSTYLE_WITH_UNDO)
+
     def insert_sample_text(self) -> None:
         """
         Insert sample text.
         :return: None
         """
-        # self._style_control.ApplyStyle(0)
-        # self.rtc.BeginParagraphStyle(Strings.style_paragraph)
+        self.rtc.BeginParagraphStyle(Strings.style_paragraph)
         self.rtc.WriteText('Example paragraph')
-        # self.rtc.Newline()
-        # self.rtc.EndParagraphStyle()
+        self.rtc.Newline()
+        self.rtc.EndParagraphStyle()
 
-        # self.rtc.BeginParagraphStyle(Strings.style_paragraph_bold)
-        # self.rtc.WriteText('Example paragraph')
-        # self.rtc.Newline()
-        # self.rtc.EndParagraphStyle()
+        self.rtc.BeginParagraphStyle(Strings.style_paragraph_bold)
+        self.rtc.WriteText('Example paragraph')
+        self.rtc.Newline()
+        self.rtc.EndParagraphStyle()
 
-        # self.rtc.BeginParagraphStyle(Strings.style_paragraph)
-        # self.rtc.WriteText('Example paragraph')
-        # self.rtc.Newline()
-        # self.rtc.EndParagraphStyle()
+        self.rtc.BeginParagraphStyle(Strings.style_paragraph)
+        self.rtc.WriteText('Example paragraph')
+        self.rtc.Newline()
+        self.rtc.EndParagraphStyle()
 
         self.rtc.Newline()
 
