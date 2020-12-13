9b74d67d8a6356b2a990a5f5bd61eb39510dc015
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2020-12-13 12:29:02 +0100

    Implement custom style changer

--- Tools/Example.py
+++ Tools/Example.py
@@ -1,39 +1,42 @@
 import wx
 import wx.richtext as rt
 
+from Constants.Constants import Strings, Numbers
+
 
 class RichTextFrame(wx.Frame):
     def __init__(self, *args, **kw):
         wx.Frame.__init__(self, *args, **kw)
         self.rtc = rt.RichTextCtrl(self, style=wx.VSCROLL | wx.HSCROLL | wx.NO_BORDER)
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self._main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._controls_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         # Create style stylesheet and control
         self._stylesheet = rt.RichTextStyleSheet()
         self._stylesheet.SetName('Stylesheet')
 
-        self._style_control = rt.RichTextStyleListBox(self, 1, size=(140, 60))
-        self._style_control.SetApplyOnSelection(True)
+        self._style_control = rt.RichTextStyleListBox(self, 1, size=(100, 160))
         self._style_control.SetStyleType(0)
         self._style_control.SetMargins(-5, -5)
+        self._style_control.Enable(False)
 
-        self.style_button = wx.Button(self, wx.ID_BOLD, 'style button bold')
-        self.style_button_bold = wx.Button(self, wx.ID_DEFAULT, 'style button normal')
+        self._style_picker = wx.ListBox(self, -1, size=(100, 160))
 
         self.rtc.SetStyleSheet(self._stylesheet)
         self._style_control.SetRichTextCtrl(self.rtc)
         self._style_control.SetStyleSheet(self._stylesheet)
 
-        self.sizer.Add(self.rtc, 1, flag=wx.EXPAND)
-        self.sizer.Add(self._style_control)
-        self.sizer.Add(self.style_button)
-        self.sizer.Add(self.style_button_bold)
-        self.SetSizer(self.sizer)
+        self._controls_sizer.Add(self._style_control)
+        self._controls_sizer.Add(self._style_picker)
 
-        self.Bind(wx.EVT_BUTTON, self._change_style, self.style_button)
-        self.Bind(wx.EVT_BUTTON, self._change_style, self.style_button_bold)
+        self._main_sizer.Add(self.rtc, 1, flag=wx.EXPAND)
+        self._main_sizer.Add(self._controls_sizer)
+        self.SetSizer(self._main_sizer)
+
+        self.Bind(wx.EVT_LISTBOX, self._change_style, self._style_picker)
 
         self._create_styles()
+        self._fill_picker()
 
     def _create_styles(self) -> None:
         """
@@ -44,12 +47,11 @@ class RichTextFrame(wx.Frame):
         stl_paragraph: rt.RichTextAttr = rt.RichTextAttr()
         stl_paragraph.SetFontSize(Numbers.paragraph_font_size)
         stl_paragraph.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
-        # todo font weight and size is ignored
         stl_paragraph.SetFontWeight(wx.FONTWEIGHT_NORMAL)
-        stl_paragraph.SetParagraphSpacingBefore(Numbers.paragraph_spacing)
-        stl_paragraph.SetParagraphSpacingAfter(Numbers.paragraph_spacing)
-        stl_paragraph.SetBackgroundColour(wx.GREEN)
-        stl_paragraph.SetParagraphStyleName(Strings.style_paragraph)
+        stl_paragraph.SetParagraphSpacingBefore(0)
+        stl_paragraph.SetParagraphSpacingAfter(0)
+
+        stl_paragraph.SetBackgroundColour(wx.YELLOW)
 
         style_paragraph: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
             Strings.style_paragraph)
@@ -57,27 +59,100 @@ class RichTextFrame(wx.Frame):
         style_paragraph.SetNextStyle(Strings.style_paragraph)
         self._stylesheet.AddParagraphStyle(style_paragraph)
 
-        # Paragraph style bold
-        stl_paragraph_bold: rt.RichTextAttr = rt.RichTextAttr()
-        stl_paragraph_bold.SetFontSize(Numbers.paragraph_font_size_1)
-        stl_paragraph_bold.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
-        stl_paragraph_bold.SetFontWeight(wx.FONTWEIGHT_BOLD)
-        stl_paragraph_bold.SetParagraphSpacingBefore(Numbers.paragraph_spacing_bold)
-        stl_paragraph_bold.SetParagraphSpacingAfter(Numbers.paragraph_spacing_bold)
-        stl_paragraph_bold.SetBackgroundColour(wx.YELLOW)
-        stl_paragraph.SetParagraphStyleName(Strings.style_paragraph_bold)
+        # Heading 3 style
+        stl_heading_3: rt.RichTextAttr = rt.RichTextAttr()
+        stl_heading_3.SetFontSize(Numbers.heading_3_size)
+        stl_heading_3.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
+        stl_heading_3.SetFontWeight(wx.FONTWEIGHT_BOLD)
+        stl_heading_3.SetParagraphSpacingBefore(Numbers.paragraph_spacing)
+        stl_heading_3.SetParagraphSpacingAfter(Numbers.paragraph_spacing)
 
-        style_paragraph_bold: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(
-            Strings.style_paragraph_bold)
-        style_paragraph_bold.SetStyle(stl_paragraph_bold)
-        style_paragraph_bold.SetNextStyle(Strings.style_paragraph)
-        self._stylesheet.AddParagraphStyle(style_paragraph_bold)
+        stl_heading_3.SetBackgroundColour(wx.RED)
+
+        style_h3: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(Strings.style_heading_3)
+        style_h3.SetStyle(stl_heading_3)
+        style_h3.SetNextStyle(Strings.style_paragraph)
+        self._stylesheet.AddParagraphStyle(style_h3)
+
+        # Heading 4 style
+        stl_heading_4: rt.RichTextAttr = rt.RichTextAttr()
+        stl_heading_4.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
+        stl_heading_4.SetFontWeight(wx.FONTWEIGHT_BOLD)
+        stl_heading_4.SetParagraphSpacingBefore(Numbers.paragraph_spacing / 2)
+        stl_heading_4.SetParagraphSpacingAfter(Numbers.paragraph_spacing / 2)
+
+        stl_heading_4.SetBackgroundColour(wx.BLUE)
+
+        style_h4: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(Strings.style_heading_4)
+        style_h4.SetStyle(stl_heading_4)
+        style_h4.SetNextStyle(Strings.style_paragraph)
+        self._stylesheet.AddParagraphStyle(style_h4)
+
+        # Image style
+        stl_image: rt.RichTextAttr = rt.RichTextAttr()
+        stl_image.SetFontSize(Numbers.paragraph_font_size)
+        stl_image.SetAlignment(wx.TEXT_ALIGNMENT_CENTER)
+        stl_image.SetFontWeight(wx.FONTWEIGHT_NORMAL)
+        stl_image.SetParagraphSpacingAfter(Numbers.image_spacing)
+        stl_image.SetParagraphSpacingBefore(Numbers.image_spacing)
+
+        style_image: rt.RichTextParagraphStyleDefinition = rt.RichTextParagraphStyleDefinition(Strings.style_image)
+        style_image.SetStyle(stl_image)
+        style_image.SetNextStyle(Strings.style_paragraph)
+        self._stylesheet.AddParagraphStyle(style_image)
+
+        # List style
+        stl_list: rt.RichTextAttr = rt.RichTextAttr()
+        stl_list.SetFontSize(Numbers.paragraph_font_size)
+        stl_list.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
+        stl_list.SetFontWeight(wx.FONTWEIGHT_NORMAL)
+        stl_list.SetParagraphSpacingBefore(Numbers.list_spacing)
+        stl_list.SetParagraphSpacingAfter(Numbers.list_spacing)
+
+        stl_list_1: rt.RichTextAttr = rt.RichTextAttr()
+        stl_list_1.SetFontSize(Numbers.paragraph_font_size)
+        stl_list_1.SetAlignment(wx.TEXT_ALIGNMENT_LEFT)
+        stl_list_1.SetFontWeight(wx.FONTWEIGHT_NORMAL)
+        stl_list.SetParagraphSpacingBefore(Numbers.list_spacing)
+        stl_list.SetParagraphSpacingAfter(Numbers.list_spacing)
+        stl_list_1.SetBulletStyle(wx.TEXT_ATTR_BULLET_STYLE_STANDARD)
+        stl_list_1.SetLeftIndent(Numbers.list_left_indent, Numbers.list_left_sub_indent)
+
+        style_list: rt.RichTextListStyleDefinition = rt.RichTextListStyleDefinition(Strings.style_list)
+        style_list.SetLevelAttributes(0, stl_list_1)
+        style_list.SetStyle(stl_list)
+        style_list.SetNextStyle(Strings.style_paragraph)
+        self._stylesheet.AddListStyle(style_list)
+
+        # Link style
+        stl_link = rt.RichTextAttr()
+        stl_link.SetURL(Strings.link_stub)
+        stl_link.SetFontUnderlined(True)
+        stl_link.SetTextColour(wx.BLUE)
+        stl_link.SetBackgroundColour(wx.RED)
+        style_link: rt.RichTextCharacterStyleDefinition = rt.RichTextCharacterStyleDefinition(Strings.style_url)
+        style_link.SetStyle(stl_link)
+        self._stylesheet.AddCharacterStyle(style_link)
 
         self.rtc.SetStyleSheet(self._stylesheet)
         self._style_control.SetRichTextCtrl(self.rtc)
         self._style_control.SetStyleSheet(self._stylesheet)
         self._style_control.UpdateStyles()
-        self.rtc.SetDefaultStyle(self._stylesheet.FindParagraphStyle(Strings.style_paragraph).GetStyle())
+
+    def _fill_picker(self) -> None:
+        """
+        Fill picker list box with style names
+        :return: None
+        """
+        names = []
+        for n in range(self._stylesheet.GetParagraphStyleCount()):
+            names.append(self._stylesheet.GetParagraphStyle(n).GetName())
+        for n in range(self._stylesheet.GetListStyleCount()):
+            names.append(self._stylesheet.GetListStyle(n).GetName())
+        for n in range(self._stylesheet.GetListStyleCount()):
+            names.append(self._stylesheet.GetCharacterStyle(n).GetName())
+
+        self._style_picker.InsertItems(names, 0)
 
     def _change_style(self, evt: wx.CommandEvent) -> None:
         """
@@ -85,11 +160,7 @@ class RichTextFrame(wx.Frame):
         :param evt: The name of the style in stylesheet
         :return: None
         """
-        if evt.GetId() == wx.ID_BOLD:
-            name = Strings.style_paragraph_bold
-        else:
-            name = Strings.style_paragraph
-        style = self._stylesheet.FindParagraphStyle(name).GetStyle()
+        style = self._stylesheet.FindStyle(evt.GetString()).GetStyle()
         p: rt.RichTextParagraph = self.rtc.GetFocusObject().GetParagraphAtPosition(self.rtc.GetCaretPosition()+1)
         self.rtc.SetStyleEx(p.GetRange().FromInternal(), style, flags=rt.RICHTEXT_SETSTYLE_RESET |
                             rt.RICHTEXT_SETSTYLE_OPTIMIZE | rt.RICHTEXT_SETSTYLE_WITH_UNDO)
@@ -104,7 +175,7 @@ class RichTextFrame(wx.Frame):
         self.rtc.Newline()
         self.rtc.EndParagraphStyle()
 
-        self.rtc.BeginParagraphStyle(Strings.style_paragraph_bold)
+        self.rtc.BeginParagraphStyle(Strings.style_heading_3)
         self.rtc.WriteText('Example paragraph')
         self.rtc.Newline()
         self.rtc.EndParagraphStyle()
@@ -117,21 +188,6 @@ class RichTextFrame(wx.Frame):
         self.rtc.Newline()
 
         self.rtc.LayoutContent()
-
-
-class Strings:
-    """
-    Just constants
-    """
-    style_paragraph: str = 'paragraph'
-    style_paragraph_bold: str = 'boldparagraph'
-
-
-class Numbers:
-    paragraph_font_size: int = 10
-    paragraph_font_size_1: int = 20
-    paragraph_spacing: int = 20
-    paragraph_spacing_bold: int = 50
 
 
 class MyApp(wx.App):
