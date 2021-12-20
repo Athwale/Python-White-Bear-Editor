97739988c5183a75963acbe5bd0c196a1bd6f216
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2021-12-20 20:26:27 +0100

    More elaborate text test

--- Experiments/test.py
+++ Experiments/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import locale
 import sys
 import gi
 gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk
 from gtkspellcheck import SpellChecker
 
@@ -14,15 +14,32 @@ class AppWindow(Gtk.ApplicationWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Layout
+        h_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=3)
+        v_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
+
         text = Gtk.TextView()
-        self.add(text)
-        self.set_size_request(width=100, height=200)
+        text.set_size_request(200, -1)
+
+        for name in ['Bold', 'Color 1', 'Color 2']:
+            button = Gtk.Button()
+            button.set_label(name)
+            button.set_size_request(200, -1)
+            button.connect("clicked", self.on_button_clicked)
+            v_box.add(button)
+
+        h_box.add(text)
+        h_box.add(v_box)
+        self.add(h_box)
+        self.set_size_request(width=400, height=400)
+        self.set_border_width(3)
 
         # Requires yum install hunspell-cs
         spellchecker = SpellChecker(text, language='cs_CZ', collapse=False)
 
-    def on_destroy(self, widget, extra_arg):
-        print(widget)
+    def on_button_clicked(self, button: Gtk.Button):
+        print(button.get_label())
 
 
 class Application(Gtk.Application):
@@ -32,7 +49,7 @@ class Application(Gtk.Application):
 
     def do_activate(self):
         if not self.window:
-            self.window = AppWindow(application=self, title="Main Window")
+            self.window = AppWindow(application=self, title="Text test")
             # Shows all widgets inside the window.
             self.window.show_all()
 
