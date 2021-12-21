949ff0d08cca40931e28625f3f9478029ca60cdb
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2021-12-21 12:34:34 +0100

    Scroll text and add buffer

--- Experiments/test.py
+++ Experiments/test.py
@@ -19,8 +19,12 @@ class AppWindow(Gtk.ApplicationWindow):
         h_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=3)
         v_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
 
-        text = Gtk.TextView()
-        text.set_size_request(200, -1)
+        self._text = Gtk.TextView()
+        self._buffer = self._text.get_buffer()
+
+        text_scrolled = Gtk.ScrolledWindow()
+        text_scrolled.set_size_request(200, -1)
+        text_scrolled.add(self._text)
 
         for name in ['Bold', 'Color 1', 'Color 2']:
             button = Gtk.Button()
@@ -29,17 +33,21 @@ class AppWindow(Gtk.ApplicationWindow):
             button.connect("clicked", self.on_button_clicked)
             v_box.add(button)
 
-        h_box.add(text)
+        h_box.add(text_scrolled)
         h_box.add(v_box)
         self.add(h_box)
         self.set_size_request(width=400, height=400)
         self.set_border_width(3)
 
         # Requires yum install hunspell-cs
-        spellchecker = SpellChecker(text, language='cs_CZ', collapse=False)
+        spellchecker = SpellChecker(self._text, language='cs_CZ', collapse=False)
+        self._write_text()
 
     def on_button_clicked(self, button: Gtk.Button):
         print(button.get_label())
+
+    def _write_text(self):
+        self._buffer.set_text('test')
 
 
 class Application(Gtk.Application):
