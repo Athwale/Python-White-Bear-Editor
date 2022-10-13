43f1a48cfc5f6c5abca803b46dd62fe33ae70e62
Ondrej Mejzlik <mejzlik.ondrej@gmail.com>
2022-10-13 20:07:37 +0200

    Return the sub image

--- Experiments/test.py
+++ Experiments/test.py
@@ -33,23 +33,27 @@ class RichTextFrame(wx.Frame):
         """
         image = wx.Image(img_path)
         image: wx.Image = image.ConvertToGreyscale()
-        bitmap = wx.StaticBitmap(self, -1, wx.Bitmap(image))
-        self.sizer.Add(bitmap)
 
+        # TODO https://discuss.wxpython.org/t/cropping-an-image-with-wxpython/34983/6
         # TODO make threshold user selectable
-        print(image.GetWidth(), image.GetHeight())
-        logo = self.prepare_image(image, limit=183)
-        bitmap = wx.StaticBitmap(self, -1, wx.Bitmap(logo))
-        self.sizer.Add(bitmap)
+        # TODO make border selectable by user
+        # TODO create logo name based on article name not filename.
+        for img in self.prepare_image(image, limit=183):
+            print(img.GetWidth(), img.GetHeight())
+            bitmap = wx.StaticBitmap(self, -1, wx.Bitmap(img))
+            self.sizer.Add(wx.StaticLine(self, -1))
+            self.sizer.Add(bitmap)
         self.sizer.Layout()
 
-    def prepare_image(self, image: wx.Image, limit: int) -> wx.Image:
+    def prepare_image(self, image: wx.Image, limit: int) -> (wx.Image, wx.Image):
         """
         Search for the beginning of an image based on red color threshold from the set direction.
         :param image: The logo to process.
         :param limit: The red color threshold where we consider the image to be useful.
         :return: Found coordinates.
         """
+        # Bounding box will be drawn only into the preview.
+        preview = image.Copy()
         # Top will be set only once on the first matching pixel.
         top = None
         # Bottom is the last matching pixel.
@@ -73,24 +77,22 @@ class RichTextFrame(wx.Frame):
                         bottom = (x, y)
                 else:
                     # Repaint all lighter colors with white
+                    preview.SetRGB(x, y, 255, 255, 255)
                     image.SetRGB(x, y, 255, 255, 255)
 
         top_left = (left[0], top[1])
         bottom_right = (right[0], bottom[1])
-        # TODO make a new blank image of the size of the selected area.
-        logo = wx.Image(100, 100, clear=True)
-        logo.Replace(0, 0, 0, 255, 255, 255)
-        for y in range(0, image.GetHeight() - 1):
-            for x in range(0, image.GetWidth() - 1):
+
+        # Draw a bounding box around the selected area.
+        for y in range(0, preview.GetHeight() - 1):
+            for x in range(0, preview.GetWidth() - 1):
                 if x == top_left[0] or x == bottom_right[0]\
                         or y == top_left[1] or y == bottom_right[1]:
-                    image.SetRGB(x, y, 255, 0, 0)
-                if top_left[0] <= x <= bottom_right[0] and top_left[1] <= y <= bottom_right[1]:
-                    # TODO Copy these pixels into a new image of appropriate size.
-                    # TODO then resize it to less than 96 pixels and pad it to 96 with white?
-                    print(x, y)
-
-        return logo
+                    preview.SetRGB(x, y, 255, 0, 0)
+        # Get the selected sub image
+        # TODO then resize it to less than 96 pixels and pad it to 96 with white?
+        crop = image.GetSubImage(wx.Rect(wx.Point(top_left), wx.Point(bottom_right)))
+        return preview, crop
 
 
 class MyApp(wx.App):
@@ -103,7 +105,7 @@ class MyApp(wx.App):
         self.frame = None
 
     def OnInit(self):
-        self.frame = RichTextFrame(None, -1, "RichTextCtrl", size=(300, 500), style=wx.DEFAULT_FRAME_STYLE)
+        self.frame = RichTextFrame(None, -1, "RichTextCtrl", size=(300, 600), style=wx.DEFAULT_FRAME_STYLE)
         self.SetTopWindow(self.frame)
         self.frame.Show()
         self.frame.process_image('/home/omejzlik/PycharmProjects/Python-White-Bear-Editor/Experiments/Screenshot.png')
